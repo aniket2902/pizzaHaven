1162ed8062755bb53dc914962ca66a51fad15812
Aniket Mahajan <aniketmahajan19@gmail.com>
2025-02-10 17:50:37 +0530

    user APIS

--- backend/spring_boot_backend_template2/src/main/java/com/pizza/repository/UserRepository.java
+++ backend/spring_boot_backend_template2/src/main/java/com/pizza/repository/UserRepository.java
@@ -10,4 +10,6 @@ import com.pizza.pojos.User;
 public interface UserRepository extends JpaRepository<User, Long>{
 	Optional<User> findByEmail(String email);
 	boolean existsByEmail(String email);
+
+
 }
--- backend/spring_boot_backend_template2/src/main/java/com/pizza/service/UserService.java
+++ backend/spring_boot_backend_template2/src/main/java/com/pizza/service/UserService.java
@@ -4,11 +4,21 @@ import com.pizza.dto.FullUserDTO;
 import com.pizza.pojos.User;
 import com.pizza.response.ApiResponse;
 
+import java.util.List;
+
 public interface UserService {
 	ApiResponse registerNewUser(FullUserDTO dto);
 
     User findUserProfileByJwt(String jwt);
 
     User findById(Long id);
+
+
+    List<User> findAll();
+
+
+    void save(User existingUser);
+
+    void delete(User user);
 }
 
